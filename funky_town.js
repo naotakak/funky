cc051dedb18f5caa3292dbba56294b9ba0fd3d2d
bleung329 <bleung@stuy.edu>
2017-12-06 13:15:06 -0500

    First commit

---
+++ funky_town.js
@@ -0,0 +1,32 @@
+var fibonacci = function(n)
+{
+	if (n==0)
+	{
+		return 0;
+	}
+	if ((n==1)||(n==2))
+	{
+		return 1;
+	}
+	else
+	{
+		return (fibonacci(n-1) + fibonacci(n-2));
+	}
+}
+
+var gcd = function(a,b)
+{
+	if ((b % a)==0)
+	{
+		return a;
+	}
+	if ((a%b) == 0)
+	{
+		return b;
+	}
+	if (a>b)
+	{
+		return gcd(a-b,b);
+	}
+	return gcd(a,b-a);
+}
